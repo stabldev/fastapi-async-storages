5ca4e5a9b85125d28d5796226649613d6120e49b
stabldev <114811070+stabldev@users.noreply.github.com>
2025-11-03 09:18:31 +0530

    chore: patch v0.1.1 release

--- src/async_storages/__init__.py
+++ src/async_storages/__init__.py
@@ -1,5 +1,5 @@
 from .base import BaseStorage, StorageFile, StorageImage
 from .s3 import S3Storage
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __all__ = ["BaseStorage", "StorageFile", "StorageImage", "S3Storage"]
