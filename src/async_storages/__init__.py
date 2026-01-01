7f0f5a645ed711f0c3b682fa711c5dca0e539ba5
stabldev <114811070+stabldev@users.noreply.github.com>
2026-01-01 23:39:23 +0530

    feat: add internal __version__ variable

--- src/async_storages/__init__.py
+++ src/async_storages/__init__.py
@@ -1,5 +1,6 @@
+from ._version import __version__ as __version__internal__
 from .base import BaseStorage, StorageFile, StorageImage
 from .s3 import S3Storage
 
-__version__ = "0.1.1"
+__version__ = __version__internal__
 __all__ = ["BaseStorage", "StorageFile", "StorageImage", "S3Storage"]
